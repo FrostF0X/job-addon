40eeeb492709d87872bb3735b917df8cca2b2b14
Dmytro Konovalov <dmytro.konowalow@gmail.com>
2018-08-22 16:07:58 +0200

    Handle http requests

--- src/main/java/com/frost_fox/jenkins/manual_job/api/OnDemandJobApiAction.java
+++ src/main/java/com/frost_fox/jenkins/manual_job/api/OnDemandJobApiAction.java
@@ -1,8 +1,24 @@
 package com.frost_fox.jenkins.manual_job.api;
 
-import hudson.model.Action;
+import hudson.Extension;
+import hudson.model.UnprotectedRootAction;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.HttpResponses;
 
-public class OnDemandJobApiAction implements Action {
+@Extension
+public class OnDemandJobApiAction implements UnprotectedRootAction {
+
+    private WorkflowJob job;
+
+
+    public OnDemandJobApiAction() {
+    }
+
+    public OnDemandJobApiAction(WorkflowJob job) {
+        this.job = job;
+    }
+
     @Override
     public String getIconFileName() {
         return null;
@@ -15,6 +31,10 @@ public class OnDemandJobApiAction implements Action {
 
     @Override
     public String getUrlName() {
-        return "onDemandJob";
+        return "on-demand-job";
+    }
+
+    public HttpResponse doIndex() {
+        return HttpResponses.plainText(job.getName());
     }
 }
