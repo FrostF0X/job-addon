f293c88976d9628458f8c05f021dcf9532d1836c
Dmytro Konovalov <dmytro.konowalow@gmail.com>
2018-08-26 21:46:17 +0200

    Style grid properly according to stage view plugin

--- on-demand-job-grid/src/main/frontend/src/grid/AddonExecutionView.js
+++ on-demand-job-grid/src/main/frontend/src/grid/AddonExecutionView.js
@@ -5,9 +5,12 @@ export default class AddonExecutionsView extends Component {
     render() {
         return (
             <td className="job-grid-addon-execution">
-                <div className="job-grid-addon-execution-cell">
-                    <div className="job-grid-addon-execution-content">
-                        {this.props.addon.name}
+                <div className="job-grid-addon-execution-cell-background">
+                    <div className="job-grid-addon-execution-cell">
+                        <div className="job-grid-addon-execution-content">
+
+                            {this.props.addon.name}
+                        </div>
                     </div>
                 </div>
             </td>
--- on-demand-job-grid/src/main/frontend/src/grid/AddonSpecificationView.js
+++ on-demand-job-grid/src/main/frontend/src/grid/AddonSpecificationView.js
@@ -4,7 +4,14 @@ import './AddonSpecificationView.css';
 export default class AddonSpecificationView extends Component {
     render() {
         return (
-            <th className="job-grid-addon-specification">{this.props.addon.name}</th>
+            <th className="job-grid-addon-specification">
+                <div className="job-grid-addon-specification-description">
+                    {this.props.addon.name}
+                </div>
+                <div className="job-grid-addon-specification-estimation">
+                    <h5 className="job-grid-addon-specification-estimation-label">20ms</h5>
+                </div>
+            </th>
         );
     }
 }
