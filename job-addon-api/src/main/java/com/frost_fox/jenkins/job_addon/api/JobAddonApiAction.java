70dcf9d57ba4d1eeb01c9ec4e643990e81b7d8de
Dmytro Konovalov <dmytro.konowalow@gmail.com>
2018-08-29 12:27:09 +0200

    Pass addons api url to frontend

--- job-addon-api/src/main/java/com/frost_fox/jenkins/job_addon/api/JobAddonApiAction.java
+++ job-addon-api/src/main/java/com/frost_fox/jenkins/job_addon/api/JobAddonApiAction.java
@@ -3,18 +3,12 @@ package com.frost_fox.jenkins.job_addon.api;
 import com.cloudbees.workflow.util.ServeJson;
 import com.frost_fox.jenkins.job_addon.addon.JobDescription;
 import com.frost_fox.jenkins.job_addon.jenkins.JenkinsJob;
-import hudson.Extension;
 import hudson.model.UnprotectedRootAction;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 
-@Extension
 public class JobAddonApiAction implements UnprotectedRootAction {
 
     private WorkflowJob job;
-
-
-    public JobAddonApiAction() {
-    }
 
     public JobAddonApiAction(WorkflowJob job) {
         this.job = job;
@@ -33,6 +27,10 @@ public class JobAddonApiAction implements UnprotectedRootAction {
     @Override
     public String getUrlName() {
         return "job-addon";
+    }
+
+    public String getFullUrl() {
+        return job.getUrl() + getUrlName();
     }
 
     @ServeJson
--- job-addon-grid/src/main/java/com/frost_fox/jenkins/job_addon_grid/JobAddonGridAction.java
+++ job-addon-grid/src/main/java/com/frost_fox/jenkins/job_addon_grid/JobAddonGridAction.java
@@ -1,16 +1,34 @@
 package com.frost_fox.jenkins.job_addon_grid;
 
+import com.frost_fox.jenkins.job_addon.api.JobAddonApiAction;
 import hudson.Extension;
 import hudson.model.Action;
 import hudson.model.InvisibleAction;
+import jenkins.model.Jenkins;
 import jenkins.model.TransientActionFactory;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 
 import javax.annotation.Nonnull;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Objects;
 
 public class JobAddonGridAction extends InvisibleAction {
+
+    private final String url;
+
+    public JobAddonGridAction(WorkflowJob job) {
+        this.url = (new JobAddonApiAction(job)).getFullUrl();
+    }
+
+    public String getApiUrl() {
+        return getJenkinsUrl() + url;
+    }
+
+    private String getJenkinsUrl() {
+        return Objects.requireNonNull(Jenkins.getInstance()).getRootUrl();
+    }
+
 
     @Extension
     public static class JobAddonActionFactory extends TransientActionFactory<WorkflowJob> {
@@ -22,7 +40,7 @@ public class JobAddonGridAction extends InvisibleAction {
 
         @Nonnull
         public Collection<? extends Action> createFor(@Nonnull WorkflowJob job) {
-            return Collections.singleton(new JobAddonGridAction());
+            return Collections.singleton(new JobAddonGridAction(job));
         }
     }
 }
